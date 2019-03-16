87ebc26f1d63d401d306ec4a722e20319ccd36bd
renshen4322 <renshen4322@sina.com>
2019-03-17 06:38:45 +0800

    99778

--- spring-cloud-config-eureka-bus/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
+++ spring-cloud-config-eureka-bus/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
@@ -4,9 +4,11 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@RefreshScope
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {

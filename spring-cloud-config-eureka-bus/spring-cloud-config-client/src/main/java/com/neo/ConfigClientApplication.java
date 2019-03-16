3f29737a5ff0cc6beaa79e042b1d8d88639de83c
renshen4322 <renshen4322@sina.com>
2019-03-17 06:53:33 +0800

    12334

--- spring-cloud-config-eureka-bus/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
+++ spring-cloud-config-eureka-bus/spring-cloud-config-client/src/main/java/com/neo/ConfigClientApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RefreshScope
 public class ConfigClientApplication {
 
